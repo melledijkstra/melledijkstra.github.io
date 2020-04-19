7c4b5c55850dc95d55576a5bde270b20cb729ecd
Melle Dijkstra <dev.melle@gmail.com>
2020-04-19 12:18:50 +0200

    fixed masonry layout

--- assets/js/stories.js
+++ assets/js/stories.js
@@ -1,3 +1,17 @@
 $(window).on('load', function() {
     $('#page-loader-wrap').fadeOut(500);
 });
+
+$(document).on('ready', function() {
+    let grid = $('#grid').masonry({
+        "columnWidth": ".grid-sizer",
+        "itemSelector": ".grid-item",
+        "percentPosition": true
+    });
+
+    grid.imagesLoaded().progress(function() {
+        // init Masonry after all images have loaded
+        grid.masonry('layout');
+    });
+
+});
